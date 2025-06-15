886b06d92cd1c7a102ffa51d23486807ab0b2ead
FNMF <1733429203@qq.com>
2025-06-15 13:10:41 +0800

    更新Enum类，调整项目结构

---
+++ apps/backend/API/Domain/Enums/LocalFileType.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Domain.Enums
+{
+    public class LocalFileType
+    {
+    }
+}
---
+++ apps/backend/API/Domain/Enums/UCStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Domain.Enums
+{
+    public enum UCStatus
+    {
+        unused = 0,
+        used = 1,
+        OT = 2,
+        invalidity = 3
+    }
+}
